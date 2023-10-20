567be13ea6eca9b043d92351ce0f44bec9459b90
01Dri <drikill01@gmail.com>
2023-10-20 03:20:44 -0300

    animação no background

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const nums = [1, 2, 3, 4];
 const contador = document.getElementById("count");
+
 let numRandom = nums[(Math.random() * nums.length | 0)];
 let numCaring = 1;
 
-function showCountScreen() {
+function showCountScreen() {    
     const animação = document.getElementById("animação");
     animação.style.top = "0";
     animação.style.opacity = "1";
@@ -13,8 +14,6 @@ function showCountScreen() {
         animação.style.top = "-30px";
         animação.style.opacity = "0";
     }, 1000);
-
-
 }
 function showMessage(value) {
     if (numRandom > 1) {
@@ -28,20 +27,18 @@ window.onload = function() {
     showMessage(numRandom);
 };
 
-let number = 0;
 function getCaring() {
-
     if (numCaring != numRandom) {
         numRandom --;  
         showMessage(numRandom);
         showCountScreen();
-    } else {
-        var text = document.getElementById('textCaring');
-        var img = document.getElementById('imgSad');
-        text.innerHTML = "Agora Mel está feliz!!!"
-        img.src = 'feliz.png';
-        var button = document.getElementById('bu');
-        button.remove();
-
+        return
     }
-}
+    var text = document.getElementById('textCaring');
+    var img = document.getElementById('imgSad');
+    text.innerHTML = "Agora Mel está feliz!!!"
+    img.src = 'feliz.png';
+    var button = document.getElementById('bu');
+    button.remove();
+
+}   
